1daf49c587517d0e0262d2998368dbf5cd6e81a4
vitalib83 <51639685+vitalib83@users.noreply.github.com>
2023-01-17 04:04:44 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/sum', methods=['POST'])
+def sum():
+    data = request.get_json()
+    num1 = data['num1']
+    num2 = data['num2']
+    return {'sum': num1 + num2}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
